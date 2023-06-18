5e451b39a5347cd5e333045d37abdaa10be6576a
jack ashton <jackdashton@outlook.com>
2023-06-18 23:15:04 +0100

    added javascript click event

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
 console.log("Connected");
+
+// Need to be able to find a specific td
+// User needs to be able to click on a td
+// innerhtml to be added to td when clicked on
+// Same time, random pc move made
+
+document.querySelector(".table").addEventListener("click", function handleClick(event) {
+  event.target.innerHTML = "X";
+  computerMove;
+});
+
+function computerMove() {
+  td.classList.add()
+};
